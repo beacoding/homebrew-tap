a79c1b1dbf364403aee3f7fd4208f3d85db98fd0
bianca <bianca.subion@gmail.com>
2016-09-10 04:15:26 -0700

    test

---
+++ goto.rb
@@ -0,0 +1,14 @@
+
+class Goto < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/biancasubion/goto/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "afa63e118896a77062195a80e5f3011a38873c4e39174c2d6e9ee267b1a2a26e"
+
+
+  def install
+      bin.install "me"
+  end
+
+end
